a3bb75be9c52f74e11138cc9c8f4f871a0cd054a
jovan <jovancraft@gmail.com>
2024-08-30 12:27:33 -0500

    minor changes

--- index.js
+++ index.js
@@ -68,3 +68,6 @@ server.delete('/hobbits/:id', (req, res) => {
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
 // the callback function passed as the second argument will run once when the server starts
 server.listen(8000, () => console.log(`API running on port 8000`));
+
+
+
